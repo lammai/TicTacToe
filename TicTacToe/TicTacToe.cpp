b10d802359f1009c5ebe41dd91e71c9d9c610095
lammai <67616507+lammai@users.noreply.github.com>
2020-07-01 19:29:07 -0700

    modified:   TicTacToe/TicTacToe.cpp modified:   TicTacToe/x64/Debug/TicTacToe.log modified:   TicTacToe/x64/Debug/TicTacToe.obj modified:   TicTacToe/x64/Debug/TicTacToe.tlog/link.read.1.tlog modified:   TicTacToe/x64/Debug/TicTacToe.tlog/link.write.1.tlog modified:   TicTacToe/x64/Debug/vc141.idb modified:   TicTacToe/x64/Debug/vc141.pdb modified:   x64/Debug/TicTacToe.exe modified:   x64/Debug/TicTacToe.ilk modified:   x64/Debug/TicTacToe.pdb

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -14,7 +14,7 @@ void displayGrid() {
 	system("cls");
 	// Display the grid
 	cout << "     0   1   2" << endl
-		<< "    ___________" << endl;
+		<< "               " << endl;
 	for (int i = 0; i < 3; i++){
 		cout << i << "  |";
 		for (int j = 0; j < 3; j++){
